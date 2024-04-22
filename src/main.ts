4919045292f263d4158639e556130c0ddbb654e4
Kevw94 <pinerokevin@icloud.com>
2024-04-22 11:23:57 +0200

    Bootstrap project with .env url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { Logger as NestLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// Check environement onfiguration
+import '@/config/env.validator';
+import { AppModule } from '@/app.module';
+import { config } from '@/config/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const PORT = config.app.port
+    const app = await NestFactory.create(AppModule, {
+		bufferLogs: true,
+	});
+	// app.useGlobalPipes(
+	// 	new ValidationPipe({
+	// 		transform: true,
+	// 		disableErrorMessages: false,
+	// 		whitelist: true,
+	// 		enableDebugMessages: true,
+	// 	}),
+	// );
+
+	// app.use(cookieParser());
+
+	// app.enableCors(corsOptionsDelegate);
+
+	await app.listen(PORT);
+
+	return app.getUrl();
 }
-bootstrap();
+
+(async (): Promise<void> => {
+	try {
+		const url = await bootstrap();
+		NestLogger.debug(`Nest application running at : ${url}`, 'Bootstrap');
+	} catch (error) {
+		NestLogger.error(error, 'Bootstrap');
+	}
+})();
