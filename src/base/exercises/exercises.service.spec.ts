a9cb9514504ad61b7792c824839f47331c4ab0d4
Kevw94 <77940775+Kevw94@users.noreply.github.com>
2024-06-27 16:02:14 +0200

    Revert "Feat/tests"

--- src/base/exercises/exercises.service.spec.ts
+++ src/base/exercises/exercises.service.spec.ts
@@ -1,17 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExercisesService } from './exercises.service';
-import { DatabaseModule } from '@/external-modules/database/mongo.module';
-import { ExercisesController } from './exercises.controller';
-import { ExercisesRepository } from './exercises.repositoy';
 
 describe('FitnessExercisesService', () => {
 	let service: ExercisesService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			imports: [DatabaseModule],
-			controllers: [ExercisesController],
-			providers: [ExercisesService, ExercisesRepository],
+			providers: [ExercisesService],
 		}).compile();
 
 		service = module.get<ExercisesService>(ExercisesService);
--- src/trainings/trainings.service.spec.ts
+++ src/trainings/trainings.service.spec.ts
@@ -1,18 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrainingsService } from './trainings.service';
-import { DatabaseModule } from '@/external-modules/database/mongo.module';
-import { TrainingsRepository } from './trainings.repository';
-import { TrainingsController } from './trainings.controller';
 
 describe('TrainingsService', () => {
 	let service: TrainingsService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			imports: [DatabaseModule],
-			providers: [TrainingsService, TrainingsRepository],
-			controllers: [TrainingsController],
-			exports: [TrainingsService],
+			providers: [TrainingsService],
 		}).compile();
 
 		service = module.get<TrainingsService>(TrainingsService);
