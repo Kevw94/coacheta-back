36ba0f5fa16f194eb64274f88f6b317bfe06bc7e
Kevw94 <pinerokevin@icloud.com>
2024-07-01 11:25:14 +0200

    Wip deploy

--- src/main.ts
+++ src/main.ts
@@ -34,16 +34,16 @@ async function bootstrap() {
 	app.use(cookieParser());
 
 	// app.enableCors(corsOptionsDelegate);
-	app.use(
-		cors({
-		  origin: 'https://0de8-2a01-e0a-cb1-f610-8461-ba4f-d08d-411f.ngrok-free.app',
-		  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-		  credentials: true,
-		}),
-	  );
+	// app.use(
+	// 	cors({
+	// 	  origin: '*',
+	// 	  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+	// 	  credentials: true,
+	// 	}),
+	//   );
 
 
-	await app.listen(PORT);
+	await app.listen(PORT, '0.0.0.0');
 
 	return app.getUrl();
 }
